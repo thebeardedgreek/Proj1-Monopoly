667b3312edd4221563e1d96a9a9ddafe0ce632c6
Anastasios Stamboulis <astamb1@students.towson.edu>
2021-02-12 21:15:39 -0500

    Task 11 Commit - Also, fixed JUnitTest Error. Didn't see the instruction to undo TYPE_CHANCE -> TYPE_CC in Task 9

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,6 +1,6 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
-public abstract class Cell {
+public abstract class Cell implements IOwnable {
 	private String name;
 	protected Player theOwner;
 	private boolean available = true;
